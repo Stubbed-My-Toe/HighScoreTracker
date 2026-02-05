63eebd372b188e9219c99f29b9870f2f3c0dd99d
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-02-05 09:19:05 -0700

    tic tac toe

--- trues_file.py
+++ trues_file.py
@@ -1,58 +1,115 @@
 # TE 2nd Tic-Tac-Toe in Pseudocode.
-
-# import random for a random number generator to select a box to put an O in.
-import random
-# Loop to loop the game if the user wants to keep going:
-while True:
-    # Board is also a list with the numbers
-    board = [1,2,3,4,5,6,7,8,9]
-    # Board is displayed as [1],[2],[3] one line down [4],[5],[6]one line down [7],[8],[9]
-    print(f"{board[0]},{board[1]},{board[2]}\n{board[3]},{board[4]},{board[5]}\n{board[6]},{board[7]},{board[8]}")
-    #Loop for player an computer turn:
-    while True:
-        # Computer variable is set to a random integer between 1 & 9
-        computer = random.randint(1,9)
-        # Player variable is set to an input where the player chooses a number on the gameboard
-        player = int(input("Choose a number from the board to place your O: "))
-        # If player selects a number in game board change it to O
-        if player in board:
-            board[player - 1] = 'O'
-        # If computer selects number in game board change it to X
-        if computer in board:
-            board[computer - 1] = 'X'
-        # Print the board after each turn
-        print(f"{board[0]},{board[1]},{board[2]}\n{board[3]},{board[4]},{board[5]}\n{board[6]},{board[7]},{board[8]}")
-        # Check for win conditions:
-    # If player gets [1],[2],[3] then they win
-    if board[0] == board[1] == board[2] == 'O':
-        print("You win!")
-        break
-    # If player gets [4],[5],[6] then they win
-    if board[3] == board[4] == board[5] == 'O':
-        print("You win!")
-        break
-    # If player gets [7],[8],[9] then they win
-    if 
-    # If player gets [1],[5],[9] then they win
-    # If player gets [7],[5],[3] then they win
-    
-    # If player gets [1],[4],[7] then they win
-    # If player gets [2],[5],[8] then they win
-    # If player gets [3],[6],[9] then they win
-
-    # If computer gets [1],[2],[3] then thy win
-    # If computer gets [4],[5],[6] then they win
-    # If comuter gets [7],[8],[9] then they win
-    
-    # If computer gets [1],[5],[9] then they win
-    # If computer gets [7],[5],[3] then they win
-    
-    # If computer gets [1],[4],[7] then they win
-    # If computer gets [2],[5],[8] then they win
-    # If computer gets [3],[6],[9] then they win
-    # If this has looped 3 times then break while loop and ask player if they want to exit or keep going.
-
-
-
-
+import time
+#TicTacToe, a game in which two players seek in alternate turns to complete a row, a column, or a diagonal with either three O's or three X's drawn in the spaces of a grid of nine squares.
+def main_game():
+    print("Hello.")
+    time.sleep(1)
+    print("")
+    print("Welcome to TicTacToe, a game in which two players seek in alternate turns to complete a row, a column, or a diagonal with either three O's or three X's drawn in the spaces of a grid of nine squares.")
+    time.sleep(2)
+    print("")
+    #Imports needed: Random
+    import random
+    #Variables needed: Board
+    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    #Restart function that lets the player choose to play again
+    def restart():
+        board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        play = input("\nWant to play again? (Say lowercase yes or no.) ")
+        if play == "no":
+            exit()
+        else:
+            game()
+    #Function that checks to see if the game is over(Win, Lose, Draw)
+    def win():
+        if board [0]==board[1]==board[2] !="":
+            if board[0] == "X":
+                print("Congratulations! You won one of the easiest game ever!")     
+                restart()
+            else:
+                print("You Lost\n\n Honestly\n\n How Did You Lose?")
+                restart()
+        if board [3]==board[4]==board[5] !="":
+            if board[3] == "X":
+                print("Congratulations! You won one of the easiest game ever!")
+                restart()
+            else:
+                print("You Lost\n\n Honestly\n\n How Did You Lose?")
+                restart()
+        if board [6]==board[7]==board[8] !="":
+            if board[6] == "X":
+                print("Congratulations! You won one of the easiest game ever!")
+                restart()
+            else:
+                print("You Lost\n\n Honestly\n\n How Did You Lose?")
+                restart()
+        if board [0]==board[4]==board[8] !="":
+            if board[0] == "X":
+                print("Congratulations! You won one of the easiest game ever!")
+                restart()
+            else:
+                print("You Lost\n\n Honestly\n\n How Did You Lose?")
+                restart()
+        if board [2]==board[4]==board[6] !="":
+            if board[2] == "X":
+                print("Congratulations! You won one of the easiest game ever!")
+                restart()
+            else:
+                print("You Lost\n\n Honestly\n\n How Did You Lose?")
+                restart()
+        if board [0]==board[3]==board[6] !="":
+            if board[0] == "X":
+                print("Congratulations! You won one of the easiest game ever!")
+                restart()
+            else:
+                print("You Lost\n\n Honestly\n\n How Did You Lose?")
+                restart()
+        if board [1]==board[4]==board[7] !="":
+            if board[1] == "X":
+                print("Congratulations! You won one of the easiest game ever!")
+                restart()
+            else:
+                print("You Lost\n\n Honestly\n\n How Did You Lose?")
+                restart()
+        if board [2]==board[5]==board[8] !="":
+            if board[2] == "X":
+                print("Congratulations! You won one of the easiest game ever!")
+                restart()
+            else:
+                print("You Lost\n\n Honestly\n\n How Did You Lose?")
+                restart()
+    #Function that checks to see if space is available (can also place in the space)
+    def space_check(plr,sym):
+        while True:
+            if board[plr-1] != "O" and board[plr-1] != "X":
+                board.pop(plr-1)
+                board.insert(plr-1, sym)
+                break
+            else:
+                if sym == "X":
+                    print("That spot was taken.")
+                    plr = int(input("Where do you want to place your 'X'"))  
+                else:
+                    plr = random.randint(1,9)
+    #Function to run the game
+    def game():
+        while True:
+        
+    #Put this in a while loop
+    #Display the board
+            print(""+board[0]+" | "+board[1]+" | "+board[2]+"\n---------\n"+board[3]+" | "+board[4]+" | "+board[5]+"\n---------\n"+board[6]+" | "+board[7]+" | "+board[8]+"")
+            print("")
+    #Let player select where to place on the board
+            choice = int(input("Where would you like to place your 'X'?\n(Please Enter a Number): "))
+    #See if the space is availabe
+            space_check(choice, "X")
+    #Check if the game is over
+            win()
+    #Computers turn
+    #See if the space is availabe
+            computerChoice = random.randint(1,9)
+            space_check(computerChoice, "O")
+    #Check if the game is over
+            win()
+main_game()
     
