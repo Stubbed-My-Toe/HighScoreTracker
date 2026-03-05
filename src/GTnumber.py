96493c962e0c7ef89191b14d52ddfa2e5cfac228
Yourinthemood <ulziibayardarin@gmail.com>
2026-03-05 09:12:22 -0700

    Clean up comments and add welcome message
    Removed commented-out game instructions and added welcome message.

--- src/GTnumber.py
+++ src/GTnumber.py
@@ -1,29 +1,8 @@
-#BEGIN Guess the Number Game
-# SETUP
-    #- The computer picks a random number between 1 and 100 (player tries to guess it)
-   # - Create a variable to count how many guesses the player makes
-   # - return score at the end
-# GAMEPLAY
-  #  - Keep asking the player for a guess until they get it right
-   # - After each wrong guess, tell them if their guess was too high or too low
-#- Add 1 to the guess count every time they guess
-# END OF GAME
-   # - When they guess correctly, tell them their scores
-   #   returns the score
-# SAMPLE OUTPUT
-  #  Enter your name: Alex
-  #  Guess a number between 1 and 100: 40
-  #  Too low, try again
-  #  Guess a number between 1 and 100: 82
-  #  Too high, try again
-  #  Guess a number between 1 and 100: 62
-  #  Yor score was 3
 
-
-
-import random
-import json
 def game():
+    import random
+    import json
+
     # SETUP
     print("Welcome to the Guess the Number Game!")
     name = input("Enter your name: ")
