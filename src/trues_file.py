373266cb3f7a9c0ff3b1cf9b2dd0e18dd5b6dbaa
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-03-05 09:16:06 -0700

    Trues file

--- src/trues_file.py
+++ src/trues_file.py
@@ -78,3 +78,40 @@ def game_menu(user):
             print("Exiting...")
             #Break
             break
+
+#Main Function
+def main_menu():
+    #While loop
+    while True:
+        #1. Log in
+        print("1. Log in")
+        #2. Create account
+        print("2. Create account")
+        #3. Exit
+        print("3. Exit")
+        #Choice is set to a user input asking them to choose 1-3.
+        main_choice = input("Choose 1-3: ")
+        #If Choice is set to 1
+        if main_choice == "1":
+            #Set user to login
+            user = log_in()
+            #If its successful print success
+            if user:
+                print("Login successful.")
+                game_menu()
+                break
+            #Else
+            else:
+                print("Incorrect Login")
+                #Print incorrect
+        #If Choice is set to 2
+        elif main_choice == "2":
+            #Run Create Account
+            create_account()
+        #If Choice is set to 3
+        elif main_choice == "3":
+            #Print Exiting...
+            print("Exiting...")
+            #Break
+            break
+main_menu()
